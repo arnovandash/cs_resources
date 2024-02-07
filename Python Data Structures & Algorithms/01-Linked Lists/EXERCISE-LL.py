afd795daf81566ca6cfc192b15cc125705db7119
arnovanwyk <arno.vanwyk1987@gmail.com>
2024-02-07 19:37:17 +0100

    Added leetcode linked list challenges

---
+++ Python Data Structures & Algorithms/01-Linked Lists/EXERCISE-LL.py
@@ -0,0 +1,544 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def make_empty(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+        
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1   
+        return True  
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        pre = self.get(index - 1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        before = None
+        after = temp.next
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+
+# Test Class Constructor
+
+my_linked_list = LinkedList(4)
+
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length)
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Head: 4
+    Tail: 4
+    Length: 1
+
+"""
+
+
+# Test Print List Method
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+
+my_linked_list.print_list()
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    1
+    2
+    3
+
+"""
+
+
+# Test Append Method
+
+my_linked_list = LinkedList(1)
+my_linked_list.make_empty()
+
+my_linked_list.append(1)
+my_linked_list.append(2)
+
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length, '\n')
+
+print('Linked List:')
+my_linked_list.print_list()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Head: 1
+    Tail: 2
+    Length: 2
+
+    Linked List:
+    1
+    2
+
+"""
+
+
+# Test Pop method
+
+def check(expect, actual, message):
+    print(message)
+    print("EXPECTED:", expect)
+    print("RETURNED:", actual)
+    print("PASS" if expect == actual else "FAIL", "\n")
+
+print("\n----- Test: Pop on linked list with one node -----\n")
+linked_list = LinkedList(1)
+linked_list.print_list()
+popped_node = linked_list.pop()
+check(1, popped_node.value, "Value of popped node:")
+check(None, linked_list.head, "Head of linked list:")
+check(None, linked_list.tail, "Tail of linked list:")
+check(0, linked_list.length, "Length of linked list:")
+
+print("\n----- Test: Pop on linked list with multiple nodes -----\n")
+linked_list = LinkedList(1)
+linked_list.append(2)
+linked_list.append(3)
+linked_list.print_list()
+popped_node = linked_list.pop()
+check(3, popped_node.value, "Value of popped node:")
+check(1, linked_list.head.value, "Head of linked list:")
+check(2, linked_list.tail.value, "Tail of linked list:")
+check(2, linked_list.length, "Length of linked list:")
+
+print("\n----- Test: Pop on empty linked list -----\n")
+linked_list = LinkedList(1)
+linked_list.head = None
+linked_list.tail = None
+linked_list.length = 0
+popped_node = linked_list.pop()
+check(None, popped_node, "Popped node from empty linked list:")
+check(None, linked_list.head, "Head of linked list:")
+check(None, linked_list.tail, "Tail of linked list:")
+check(0, linked_list.length, "Length of linked list:")
+
+print("\n----- Test: Pop all -----\n")
+linked_list = LinkedList(1)
+linked_list.append(2)
+linked_list.print_list()
+popped_node = linked_list.pop()
+check(2, popped_node.value, "Value of popped node (first pop):")
+check(1, linked_list.head.value, "Head of linked list (after first pop):")
+check(1, linked_list.tail.value, "Tail of linked list (after first pop):")
+check(1, linked_list.length, "Length of linked list (after first pop):")
+popped_node = linked_list.pop()
+check(1, popped_node.value, "Value of popped node (second pop):")
+check(None, linked_list.head, "Head of linked list (after second pop):")
+check(None, linked_list.tail, "Tail of linked list (after second pop):")
+check(0, linked_list.length, "Length of linked list (after second pop):")
+popped_node = linked_list.pop()
+check(None, popped_node, "Popped node from empty linked list (third pop):")
+check(None, linked_list.head, "Head of linked list (after third pop):")
+check(None, linked_list.tail, "Tail of linked list (after third pop):")
+check(0, linked_list.length, "Length of linked list (after third pop):")
+
+
+# Test Prepend Method
+
+my_linked_list = LinkedList(2)
+my_linked_list.append(3)
+
+print('Before prepend():')
+print('----------------')
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length, '\n')
+print('Linked List:')
+my_linked_list.print_list()
+
+
+my_linked_list.prepend(1)
+
+
+print('\n\nAfter prepend():')
+print('---------------')
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length, '\n')
+print('Linked List:')
+my_linked_list.print_list()
+
+
+
+"""
+    EXPECTED OUTPUT:
+
+    Before prepend():
+    ----------------
+    Head: 2
+    Tail: 3
+    Length: 2
+
+    Linked List:
+    2
+    3
+
+
+    After prepend():
+    ---------------
+    Head: 1
+    Tail: 3
+    Length: 3
+
+    Linked List:
+    1
+    2
+    3
+
+"""
+
+
+# Test Pop First Method
+
+my_linked_list = LinkedList(2)
+my_linked_list.append(1)
+
+
+# (2) Items - Returns 2 Node
+print(my_linked_list.pop_first().value)
+# (1) Item -  Returns 1 Node
+print(my_linked_list.pop_first().value)
+# (0) Items - Returns None
+print(my_linked_list.pop_first())
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    2
+    1
+    None
+
+"""
+
+
+# Test Get Method
+
+my_linked_list = LinkedList(0)
+my_linked_list.append(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+
+print(my_linked_list.get(3).value)
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    3
+
+"""
+
+
+# Test Set Method
+
+my_linked_list = LinkedList(11)
+my_linked_list.append(3)
+my_linked_list.append(23)
+my_linked_list.append(7)
+
+print('LL before set_value():')
+my_linked_list.print_list()
+
+my_linked_list.set_value(1,4)
+
+print('\nLL after set_value():')
+my_linked_list.print_list()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    LL before set_value():
+    11
+    3
+    23
+    7
+
+    LL after set_value():
+    11
+    4
+    23
+    7
+"""
+
+
+# Test Insert Method
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(3)
+
+
+print('LL before insert():')
+my_linked_list.print_list()
+
+
+my_linked_list.insert(1,2)
+
+print('\nLL after insert(2) in middle:')
+my_linked_list.print_list()
+
+
+my_linked_list.insert(0,0)
+
+print('\nLL after insert(0) at beginning:')
+my_linked_list.print_list()
+
+
+my_linked_list.insert(4,4)
+
+print('\nLL after insert(4) at end:')
+my_linked_list.print_list()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    LL before insert():
+    1
+    3
+
+    LL after insert(2) in middle:
+    1
+    2
+    3
+
+    LL after insert(0) at beginning:
+    0
+    1
+    2
+    3
+
+    LL after insert(4) at end:
+    0
+    1
+    2
+    3
+    4
+
+"""
+
+
+# Test Remove Method
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+
+print('LL before remove():')
+my_linked_list.print_list()
+
+print('\nRemoved node:')
+print(my_linked_list.remove(2).value)
+print('LL after remove() in middle:')
+my_linked_list.print_list()
+
+print('\nRemoved node:')
+print(my_linked_list.remove(0).value)
+print('LL after remove() of first node:')
+my_linked_list.print_list()
+
+print('\nRemoved node:')
+print(my_linked_list.remove(2).value)
+print('LL after remove() of last node:')
+my_linked_list.print_list()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    LL before remove():
+    1
+    2
+    3
+    4
+    5
+
+    Removed node:
+    3
+    LL after remove() in middle:
+    1
+    2
+    4
+    5
+
+    Removed node:
+    1
+    LL after remove() of first node:
+    2
+    4
+    5
+
+    Removed node:
+    5
+    LL after remove() of last node:
+    2
+    4
+
+"""
+
+
+# Test Reverse Method 
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+
+print('LL before reverse():')
+my_linked_list.print_list()
+
+my_linked_list.reverse()
+
+print('\nLL after reverse():')
+my_linked_list.print_list()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    LL before reverse():
+    1
+    2
+    3
+    4
+
+    LL after reverse():
+    4
+    3
+    2
+    1
+    
+"""

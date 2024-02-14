85e6285ba67be30b052398014d46d028a4550591
arnovanwyk <arno.vanwyk1987@gmail.com>
2024-02-14 14:48:58 +0100

    Added hash tables, heaps and graphs

---
+++ Python Data Structures & Algorithms/06-Hash Tables/EXERCISE-HT.py
@@ -0,0 +1,102 @@
+class HashTable:
+    def __init__(self, size=7):
+        self.data_map = [None] * size
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
+
+    def print_table(self):
+        for i, val in enumerate(self.data_map):
+            print(i, ": ", val)
+
+    def set_item(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
+
+    def keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
+
+
+# Test set Method
+
+my_hash_table = HashTable()
+
+my_hash_table.set_item('bolts', 1400)
+my_hash_table.set_item('washers', 50)
+my_hash_table.set_item('lumber', 70)
+
+my_hash_table.print_table()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    0 :  None
+    1 :  None
+    2 :  None
+    3 :  None
+    4 :  [['bolts', 1400], ['washers', 50]]
+    5 :  None
+    6 :  [['lumber', 70]]
+
+"""
+
+
+# Test get Method
+
+my_hash_table = HashTable()
+
+my_hash_table.set_item('bolts', 1400)
+my_hash_table.set_item('washers', 50)
+
+print('Bolts:', my_hash_table.get_item('bolts'))
+print('Washers:', my_hash_table.get_item('washers'))
+print('Lumber:', my_hash_table.get_item('lumber'))
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Bolts: 1400
+    Washers: 50
+    Lumber: None
+
+"""
+
+
+# Test keys Method
+
+my_hash_table = HashTable()
+
+my_hash_table.set_item('bolts', 1400)
+my_hash_table.set_item('washers', 50)
+my_hash_table.set_item('lumber', 70)
+
+print(my_hash_table.keys())
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    ['bolts', 'washers', 'lumber']
+
+"""

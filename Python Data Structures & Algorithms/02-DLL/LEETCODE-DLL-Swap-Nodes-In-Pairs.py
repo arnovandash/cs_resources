b122ace17e2b4942ee5b544d106f68a2ad51d41c
arnovanwyk <arno.vanwyk1987@gmail.com>
2024-02-16 11:55:17 +0100

    Added DLL leetcode solution

---
+++ Python Data Structures & Algorithms/02-DLL/LEETCODE-DLL-Swap-Nodes-In-Pairs.py
@@ -0,0 +1,84 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.length = 1
+
+    def print_list(self):
+        output = []
+        current_node = self.head
+        while current_node is not None:
+            output.append(str(current_node.value))
+            current_node = current_node.next
+        print(" <-> ".join(output))
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            temp = self.head
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = new_node
+            new_node.prev = temp
+        self.length += 1
+        return True
+
+    def swap_pairs(self):
+        if not self.head or not self.head.next:
+            return
+        prev = None
+        current_node = self.head
+
+        self.head = current_node.next
+
+        while current_node and current_node.next:
+            # Working nodes
+            node1 = current_node
+            node2 = current_node.next
+
+            # Swap next pointers
+            node1.next = node2.next
+            node2.next = node1
+
+            # Swap prev pointers
+            node1.prev = node2
+            if prev:
+                prev.next = node2
+            node2.prev = prev
+
+            prev = node1
+            current_node = node1.next
+
+
+my_dll = DoublyLinkedList(1)
+my_dll.append(2)
+my_dll.append(3)
+my_dll.append(4)
+
+print('my_dll before swap_pairs:')
+my_dll.print_list()
+
+my_dll.swap_pairs()
+
+print('my_dll after swap_pairs:')
+my_dll.print_list()
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    my_dll before swap_pairs:
+    1 <-> 2 <-> 3 <-> 4
+    ------------------------
+    my_dll after swap_pairs:
+    2 <-> 1 <-> 4 <-> 3
+
+"""

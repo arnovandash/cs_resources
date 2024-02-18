d2b11a866dde5f1e407c755842120496a30b967f
arnovanwyk <arno.vanwyk1987@gmail.com>
2024-02-18 22:01:17 +0100

    Added hash table problems

---
+++ Python Data Structures & Algorithms/06-Hash Tables/LEETCODE-HT-Find-Duplicates-1.py
@@ -0,0 +1,33 @@
+def find_duplicates(nums):
+    my_dict = {}
+    duplicates = []
+    for i in nums:
+        if i in my_dict:
+            duplicates.append(i)
+        my_dict[i] = True
+    return duplicates
+
+
+
+print ( find_duplicates([1, 2, 3, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 3]) )
+print ( find_duplicates([1, 1, 1, 1, 1]) )
+print ( find_duplicates([1, 2, 3, 3, 3, 4, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([1, 1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([]) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    []
+    [1, 2]
+    [1]
+    [3, 4]
+    [1, 2, 3]
+    [1, 2, 3]
+    []
+
+"""
